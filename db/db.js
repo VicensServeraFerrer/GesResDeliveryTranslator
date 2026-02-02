d97b75ebb48e9089e1f49c4bed60120147f0f157
Vicenç Servera <vserveraferrer@gmail.com>
2026-02-02 22:52:20 +0100

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
+// UNCOMMENT FOR LOCAL TESTING
+// const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+//     host: process.env.DATABASE_HOST,
+//     dialect: 'postgres',
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
 });
 
 (async () => {
