7d869e474af259e7a9f0b33480d31ee4a256f902
Vicenç Servera <vserveraferrer@gmail.com>
2026-02-06 22:31:02 +0100

    started ping manage

--- routes/purchase_gumroad.js
+++ routes/purchase_gumroad.js
@@ -1,6 +1,6 @@
 import 'dotenv/config.js'
 import express from "express"
-import { AccessToken, sequelize } from '../models/index.js'
+import { AccessToken, Customer, User, Subscription } from '../models/index.js'
 import { requireAuthAPI } from '../helpers/authSession.js';
 import { create_token, sha256 } from '../helpers/encrypt.js';
 
@@ -11,7 +11,34 @@ gumroadRouter.use(express.urlencoded({ extended: false }));
 gumroadRouter.post("/ping", async (req, res) => {
   const payload = req.body;
 
+  if(payload.permalink != "traductor") {
+    return res.status(200).json({message: "Wrong product"})
+  }
 
+  try {
+    const customer = await Customer.create({
+      gumroadSaleId: payload.sale_id,
+      email: payload.email,
+      userId: null
+    });
+
+    const user = await User.create({
+      email: payload.email,
+      status: 'avtive',
+      customerId: customer.id
+    });
+
+    Customer.update(
+      { userId: user.userId },
+      {
+        where: { id: customer.id }
+      }
+    );
+
+
+  } catch (err) {
+    
+  }
   // Ejemplos de campos que suelen venir:
   // payload.email, payload.product_id, payload.sale_id, payload.license_key, payload.url_params, etc.
   // (Depende del tipo de producto/venta)
